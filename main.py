be560fd1136c66cdebc11416259a59743280076a
martin <martin-cleven@gmx.net>
2021-03-04 20:40:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# from torch.distributions import transforms
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from src.data.dataset import FacialKeypointsDataset
+from src.data.transforms import Rescale, RandomCrop, Normalize, ToTensor
+from src.model.models import Net
+
+data_transform = transforms.Compose([Rescale(250),
+                                     RandomCrop(224),
+                                     Normalize(),
+                                     ToTensor()])
+"""
+
+    Convert the face from RGB to grayscale
+    Normalize the grayscale image so that its color range falls in [0,1] instead of [0,255]
+    Rescale the detected face to be the expected square size for your CNN (224x224, suggested)
+    Reshape the numpy image into a torch image.
+
+"""
+
+# Construct the dataset
+face_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keypoints.csv',
+                                      root_dir='data/training/',
+                                      transform=None)
+
+test_dataset = FacialKeypointsDataset(csv_file='data/test_frames_keypoints.csv',
+                                      root_dir='data/test/',
+                                      transform=data_transform)
+
+# print some stats about the dataset
+print('Length of dataset: ', len(face_dataset))
+
+
+# load training data in batches
+batch_size = 10
+
+test_loader = DataLoader(test_dataset,
+                         batch_size=batch_size,
+                         shuffle=True,
+                         num_workers=4)
+
+train_loader = DataLoader(face_dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=4)
+
+for sample in test_loader:
+    net = Net()
+    image = sample['image']
+    print(image.shape)
+    print(sample['keypoints'].shape)
+    prediction = net(image)
+    print(prediction.shape)
+    break
---
+++ src/model/cnn.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        batch_size = x.shape[0]
+        return x.view(batch_size, -1)
+
+
+class ToKeyPoints(nn.Module):
+    def forward(self, x):
+        batch_size = x.shape[0]
+        return x.view(batch_size, -1, 2)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 10, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(10, 16, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.3),
+
+            Flatten(),
+            # # ToDo: more features so we can add a second linear layer
+            nn.Linear(46656, 2 * 68),
+
+            ToKeyPoints()
+        )
+
+    def forward(self, x):
+
+        return self.main(x)
+
+
+if __name__ == '__main__':
+    import torch
+    # instantiate and print your Net
+    net = Net()
+    print(net)
+
+    t = torch.rand([20, 1, 224, 224])
+    print(t.shape)
+    print(net(t).shape)
